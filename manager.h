ba7aa5842527e67bcbc38b601ae385cb2f95470b
newwin01 <newwin0198@handong.ac.kr>
2022-04-13 11:01:11 +0900

    Add for selecting Data number

--- manager.h
+++ manager.h
@@ -9,4 +9,4 @@ void searchProductWeight_poly(Product *p,int count);//제품의 무게 검색을
 int printFinalPrice(Product *p,int count);//최종 가격을 출력한다. 최종가격과 적립금을 빼서 출력한다. 
 int loadData(Product *p);//파일 형태의 제품 정보를 입력한다.
 void saveData(Product *p, int count);//파일 형태로 제품 정보를 추가한다.
-
+int selectDataNo(Product *p, int count);//특정 번호의 제품 정보를 출력한다.
