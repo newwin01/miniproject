a2e33491e8e5fb6394d8c0dae39e5c8a62e7bf97
newwin01 <newwin0198@handong.ac.kr>
2022-04-13 11:08:43 +0900

    search explanation funtion is added

--- manager.c
+++ manager.c
@@ -14,11 +14,64 @@ void listProduct(Product *p,int count){
     }
 }
 
-
 int printFinalPrice(Product *p,int count){
     int final=0;
     int money=0;
     for(int i=0;i<count;i++){
         if(p[i].price==-1) continue;
-
+        money=p[i].price+money;
+        final=p[i].user_money+final;
+        #ifdef DEBUG
+            printf("%d %d ",money,final);
+        #endif
+    }
+    
+    return (money-final);
+}
+void searchProductName_poly(Product *p,int count){
+    char search[MAX];
+    int scount=0;
+    getchar();
+    printf("제품 이름:");
+    scanf("%[^\n]s",search);
+    printf("\n***************\n");
+    for(int i=0;i<count;i++){
+        if(p[i].price==-1) continue;
+        #ifdef DEBUG
+            printf("\nDEBUG\n");
+            readProduct(p[i]);
+             printf("===================\n");
+        #endif
+        if(strstr(p[i].product_name,search)){
+        printf("\n");
+        readProduct(p[i]);
+        scount++;
+        }
+    }
+    if(scount==0)
+    printf("데이터 없음!\n");
+}
+void searchproductExp_poly(Product *p,int count){//제품의 설명 검색을 이용해 물품 정보를 출력한다.
+ char search[MAX];
+    int scount=0;
+    getchar();
+    printf("제품 설명:");
+    scanf("%[^\n]s",search);
+    printf("\n***************\n");
+    for(int i=0;i<count;i++){
+        if(p[i].price==-1) continue;
+        #ifdef DEBUG
+            printf("\nDEBUG\n");
+            readProduct(p[i]);
+             printf("===================\n");
+        #endif
+        if(strstr(p[i].product_exp,search)){
+        printf("\n");
+        readProduct(p[i]);
+        scount++;
+        }
+    }
+    if(scount==0)
+    printf("데이터 없음!\n");
+}
 
