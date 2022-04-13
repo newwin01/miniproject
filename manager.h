397b8ad08921bfc0b61655f09f6c8e8a12217d0e
newwin01 <newwin0198@handong.ac.kr>
2022-04-13 10:57:32 +0900

    Add explanation search

--- manager.h
+++ manager.h
@@ -2,6 +2,7 @@
 
 
 void listProduct(Product *p,int count);//모든 프로덕트의 정보를 출력한다. 
+void searchproductExp_poly(Product *p,int count);//제품의 설명 검색을 이용해 물품 정보를 출력한다.
 void searchProductName_poly(Product *p,int count);//제품의 이름 검색을 이용해 물품 정보를 출력한다. 
 int printFinalPrice(Product *p,int count);//최종 가격을 출력한다. 최종가격과 적립금을 빼서 출력한다. 
 
